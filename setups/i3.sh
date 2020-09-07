6b258fee8574eac47e03b734be46b7286aa22aa3
Hugo Moreau <hugo.moreau@epita.fr>
2020-09-07 14:50:01 +0200

    i3 setup added
    i3 now can be installed with setups bash script.
    
     * i3/i3blocks.conf,
       i3/i3status.conf,
       i3/scripts/*,
       setups/i3.sh : Here.

---
+++ setups/i3.sh
@@ -0,0 +1,21 @@
+# Install i3, i3lock-color, i3blocks
+sudo pacman -S i3-gaps i3lock-color i3blocks
+
+# Install URxvt
+sudo pacman -S rxvt-unicode
+
+# Install pavucontrol, playerctl
+sudo pacman -S pavucontrol playerctl
+
+# Link i3 config files
+rm -rf ~/.config/i3/*
+ln -s ~/dotfiles/i3/config ~/.config/i3/config
+ln -s ~/dotfiles/i3/i3status.conf ~/.config/i3/i3status.conf
+ln -s ~/dotfiles/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
+ln -s ~/dotfiles/i3/scripts ~/.config/i3/scripts
+ln -s ~/dotfiles/i3/i3lock ~/.config/i3/i3lock
+
+#Install screenshot supports
+sudo pacman -S xclip scrot
+
+xrandr --output eDP1 --primary
