975c2f0bea8647078058759e6021f7258f6c62b7
Hugo Moreau <hugo.moreau@epita.fr>
2020-09-07 14:31:38 +0200

    Rofi config added
    rofi config files added and script to install done
    Including calculator
    
     * rofi/config.rasi,
       rofi/custom_nord.rasi,
       setups/rofi.sh : Here.

---
+++ setups/rofi.sh
@@ -0,0 +1,24 @@
+# Install rofi and compton
+sudo pacman -S rofi compton
+
+# Install rofi themes
+git clone https://github.com/davatorium/rofi-themes.git
+cd rofi-themes
+./install
+cd ..
+rm -rf rofi-themes
+ln -s ~/dotfiles/rofi/config.rasi ~/.config/rofi/config.rasi
+ln -s ~/dotfiles/rofi/custom_nord.rasi ~/.local/share/rofi/themes/
+
+# Install calculate
+sudo pacman -S libqalculate libtool
+git clone git@github.com:svenstaro/rofi-calc.git
+cd rofi-calc
+autoreconf -i
+mkdir build
+cd build
+../configure
+make
+sudo make install
+cd ../..
+rm -rf rofi-calc
