916d4e26c2fa32adcf9f5ab6ee5b8e06fd631d5d
Hugo Moreau <hugo.moreau@epita.fr>
2020-09-08 19:20:48 +0200

    Install Chewie snippets for c
     * setups/vim.sh : Here.

--- setups/vim.sh
+++ setups/vim.sh
@@ -1,5 +1,5 @@
 # Install vim
-sudo pacman -S vim
+#sudo pacman -S vim
 
 # Install Vundle package manager for vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -16,3 +16,8 @@ cd fonts
 ./install.sh
 cd ..
 rm -rf fonts
+
+# Install Chewie EPITA C snippets
+git clone https://github.com/Chewie/EPITA-snippets.git
+cp EPITA-snippets/c_EPITA.snippets ~/.vim/bundle/ultisnips/
+rm -rf EPITA-snippets/
