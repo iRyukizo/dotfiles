141d6dd69b5638354ea9e592b97ed30403ace427
Hugo Moreau <hugo.moreau@epita.fr>
2020-09-18 19:49:45 +0200

    Handling multiple keyboard
    mod+Shift+u for us kb,
    mod+Shift+f for fr kb.
    
     * i3/config*,
       i3/scripts/fr,
       i3/scripts/us,
       polybar/config,
       setups/i3*.sh : Here.

--- setups/i3.sh
+++ setups/i3.sh
@@ -10,6 +10,8 @@ sudo pacman -S pavucontrol playerctl
 # Link i3 config files
 rm -rf ~/.config/i3/*
 ln -s ~/dotfiles/i3/config ~/.config/i3/config
+ln -s ~/dotfiles/i3/configus ~/.config/i3/configus
+ln -s ~/dotfiles/i3/configfr ~/.config/i3/configfr
 ln -s ~/dotfiles/i3/i3status.conf ~/.config/i3/i3status.conf
 ln -s ~/dotfiles/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
 ln -s ~/dotfiles/i3/scripts ~/.config/i3/scripts
--- setups/i3reconf.sh
+++ setups/i3reconf.sh
@@ -1,6 +1,8 @@
 # Reinstall properly i3 after second login
 rm -rf ~/.config/i3/*
 ln -s ~/dotfiles/i3/config ~/.config/i3/config
+ln -s ~/dotfiles/i3/configus ~/.config/i3/configus
+ln -s ~/dotfiles/i3/configfr ~/.config/i3/configfr
 ln -s ~/dotfiles/i3/i3status.conf ~/.config/i3/i3status.conf
 ln -s ~/dotfiles/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
 ln -s ~/dotfiles/i3/scripts ~/.config/i3/scripts
